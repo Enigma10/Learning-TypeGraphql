95cd17b25af9cf6d400250e3884dbf577c130488
Enigma10 <vkrsaini678@gmail.com>
2019-01-17 01:56:08 +0530

    rename file

--- src/modules/user/register/RegisterInput.ts
+++ src/modules/user/register/RegisterInput.ts
@@ -3,10 +3,9 @@ import { Length, IsEmail } from "class-validator";
 import { Field, InputType } from "type-graphql";
 import { IsEmailAlreadyExist } from "./isEmailAlreadyExist";
 import { PasswordMixin } from "../../shared/PasswordInput";
-import { OkMixin } from "../../shared/OkMixin";
 
 @InputType()
-export class RegisterInput extends OkMixin(PasswordMixin(class {})) {
+export class RegisterInput extends PasswordMixin(class {}) {
   @Field()
   @Length(1, 255)
   firstName: string;
